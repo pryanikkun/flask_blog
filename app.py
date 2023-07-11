26c8a51b86bd88c1fb0b0aa8453d4094433b9b7e
Name LastName <name56464@domen.ru>
2023-07-11 18:49:32 +1000

    edited app.py, added create()

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 
 app = Flask(__name__, template_folder='templates/')
+app.config['SECRET_KEY'] = 'itslo0o0o0o0ngsecretkey'
 
 
 def get_db_connection():
@@ -33,3 +34,21 @@ def index():
 def post_(post_id):
     post = get_post(post_id)
     return render_template('post.html', post=post)
+
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+    return render_template('create.html')
