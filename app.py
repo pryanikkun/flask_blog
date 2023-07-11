472be1657cd7a092a11714a3a7769fa10d3d9341
Name LastName <name56464@domen.ru>
2023-07-11 18:12:22 +1000

    edited app.py, added get_post(), post_()

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import sqlite3
 from flask import Flask, render_template
+from werkzeug.exceptions import abort
 
 app = Flask(__name__, template_folder='templates/')
 
@@ -10,9 +11,25 @@ def get_db_connection():
     return conn
 
 
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id=?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
+
 @app.route('/')
 def index():
     conn = get_db_connection()
     posts = conn.execute('SELECT * FROM posts').fetchall()
     conn.close()
     return render_template('index.html', posts=posts)
+
+
+@app.route('/<int:post_id>')
+def post_(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
